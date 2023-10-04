a6cd9d6ca0940d6b5a65e1dc8256f87e3488319e
rhnsh <114548529+rhnsh@users.noreply.github.com>
2023-10-04 18:27:17 +0700

    Add files via upload

---
+++ Tugas_Sesi2_MRaihan.js
@@ -0,0 +1,12 @@
+const firstName = "Muhammad"
+const lastName = "Raihan"
+const age = 19;
+const isMarried = false;
+
+
+//Menampilkan nilai variabel
+
+console.log("Nama Depan:", firstName);
+console.log("Nama Belakang:", lastName);
+console.log("Umur:", age);
+console.log("Status Pernikahan:", isMarried);
